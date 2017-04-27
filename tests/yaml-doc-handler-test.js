d34826b7c0fc13f8f41e10eb1eab92cef16b62ac
Stuart McLean <s.mclean@foodpanda.com>
2017-04-27 10:38:44 +0200

    WIP - tests passing

--- tests/yaml-doc-handler-test.js
+++ tests/yaml-doc-handler-test.js
@@ -10,14 +10,20 @@ var fs = require('fs'),
 describe('allow object-like access to swagger files using $ref notation', function () {
     describe('getArrayForKey', function () {
         it('should find a simple key in the object', function () {
+            var simpleObject = {'foo': {'bar': 'baz'}};
+            docHandler.getArrayForKey('foo', simpleObject).should.eql({'bar': 'baz'});
+        });
+
+        it('should return an empty object if key does not exist', function () {
             var simpleObject = {'foo': 'bar'};
-            docHandler.getArrayForKey('foo', simpleObject).should.eql('bar');
-        })
+            var result = docHandler.getArrayForKey('baz', simpleObject);
+            Object.keys(result).length.should.eql(0);
+        });
     });
 
     describe('getArrayForMultipleKeys', function () {
         it('should recursively find keys in a simple object', function () {
-            var simpleObject = {'foo': {'bar': 'baz'}}
+            var simpleObject = {'foo': {'bar': 'baz'}};
             docHandler.getArrayForMultipleKeys(['foo', 'bar'], simpleObject).should.eql('baz');
         })
     });
